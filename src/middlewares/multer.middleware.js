09f12a086cc1f2160518c593c0597639eab3c94b
purijatinn@gmail.com <purijatinn@gmail.com>
2025-01-04 14:06:00 +0530

    add comment to multer middleware for clarity on filename generation

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -4,6 +4,7 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/tmp");
   },
+  //for github commit
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, file.fieldname + "-" + uniqueSuffix);
